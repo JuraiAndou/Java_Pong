6a363e0cd3fe1a51ba8a2e89ffa2d2f2040ae43e
Samuel Afonso Gomes <samuel.gomes.afonso@gmail.com>
2023-07-08 17:30:03 -0300

    entity corrention

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -2,13 +2,11 @@ package game;
 
 public class Entity implements Graphics, Physics{
 
-	@Override
 	public void update(double delta) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
 		
