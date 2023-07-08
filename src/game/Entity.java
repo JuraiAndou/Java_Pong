06c176e0c2bf1bc0af9d75b405064bb7193502a3
Samuel Afonso Gomes <samuel.gomes.afonso@gmail.com>
2023-07-08 18:08:29 -0300

    Entity class correction

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -2,12 +2,13 @@ package game;
 
 
 public class Entity implements Graphics, Physics{
-
+	@Override
 	public void update(double delta) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void draw(java.awt.Graphics g) {
 		// TODO Auto-generated method stub
 		
