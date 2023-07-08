e6e1b47fe9eeaa52a0d152af2bd75a0f8df7c497
Rian Victor <rianvictor0102@gmail.com>
2023-07-08 16:43:49 -0300

    pinto

--- src/game/Graphics.java
+++ src/game/Graphics.java
@@ -1,5 +1,9 @@
 package game;
 
-public interface Graphics {
+//Interface grafica. Define que todas as classes que a implementam obrigatoriamente deveram ter o método draw()
 
+public interface Graphics {
+	
+	//Método responsavel por "desenhar" as entidades na tela. Como cada entidade possui uma "aparencia" diferente, a implementação fica por conta delas.
+	public void draw();
 }
