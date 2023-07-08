c042c905157eb26624b1ae8a3ffda1b7d98e0b0e
Rian Victor <rianvictor0102@gmail.com>
2023-07-08 17:13:16 -0300

    Vo cometar tudo fds

---
+++ src/game/Entity.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Entity implements Graphics, Physics{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/game/Player.java
@@ -0,0 +1,20 @@
+package game;
+
+public class Player extends Entity{
+	public Player(int width, int height, int Scale){
+		System.out.println("Player initialized....");
+		int score = 0;
+	}
+
+	@Override
+	public void update() {
+		
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+}
